3157870a6e52b600fc83f560c1293a859e9140bb
Felipe Rodrigues <felipe.rodrigues@edu.pucrs.br>
2022-06-21 17:04:12 -0300

    criacao do array bidimensional de Strings com os dados de aves.

--- Ave.java
+++ Ave.java
@@ -11,5 +11,56 @@
 
 
 class Ave{
+	
+	public static void main(String[] args) {
+
+	
+	String[][] catalogoAves;
+	
+	catalogoAves = new String[10][3];
+	
+	catalogoAves[0][0] = "pica-pau-branco";
+	catalogoAves[0][1] = "branco";
+	catalogoAves[0][2] = "campo com arores, arbustos ou arvoretas";
+	
+	catalogoAves[1][0] = "noivinha-coroada";
+	catalogoAves[1][1] = "branco";
+	catalogoAves[1][2] = "campo com arores, arbustos ou arvoretas";
+	
+	catalogoAves[2][0] = "cardeal";
+	catalogoAves[2][1] = "branco";
+	catalogoAves[2][2] = "campo com arores, arbustos ou arvoretas";
+	
+	catalogoAves[3][0] = "tijerila";
+	catalogoAves[3][1] = "branco";
+	catalogoAves[3][2] = "campo com arores, arbustos ou arvoretas";
+	
+	catalogoAves[4][0] = "grauna";
+	catalogoAves[4][1] = "preto";
+	catalogoAves[4][2] = "campo seco baixo";
+	
+	catalogoAves[5][0] = "cardeal-do-banhado";
+	catalogoAves[5][1] = "preto";
+	catalogoAves[5][2] = "banhado com espelho d'agua";
+	
+	catalogoAves[6][0] = "sargento";
+	catalogoAves[6][1] = "preto";
+	catalogoAves[6][2] = "banhado com espelho d'agua";
+	
+	catalogoAves[7][0] = "tiziu";
+	catalogoAves[7][1] = "preto";
+	catalogoAves[7][2] = "campo com arores, arbustos ou arvoretas";
+	
+	catalogoAves[8][0] = "batuíra-de-papo-ferrugíneo";
+	catalogoAves[8][1] = "cinza";
+	catalogoAves[8][2] = "campo seco baixo";
+	
+	catalogoAves[9][0] = "pomba-do-orvalho";
+	catalogoAves[9][1] = "cinza";
+	catalogoAves[9][2] = "campo com arores, arbustos ou arvoretas";
+	
+	
+		
+	}
 
 }
