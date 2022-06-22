8124a9622321d786daeefea6a7e7a0330d94cfe0
Vicente Hofmeister <vicentehofmeister@gmail.com>
2022-06-22 10:36:25 -0300

    .

--- FontanaApp.java
+++ FontanaApp.java
@@ -15,35 +15,9 @@ public class FontanaApp{
 
       public static void main(String args[]){
 
-            Scanner sc = new Scanner(System.in);
-            //Ave ave1 = new Ave();
-            //Anotacao caderno = new Anotacao();
-            int userAnswer;
+            Ave ave1 = new Ave("passarinho");
 
-            System.out.printf("\nFontanaApp!!\n\n");
-            System.out.printf("O que você deseja fazer?(1 ou 2)\n");
-
-            userAnswer = sc.nextInt();
-
-            while (userAnswer != 1 && userAnswer != 2){
-                  System.out.printf("\nResposta inválida, tente novamente\n");
-                  userAnswer = sc.nextInt();
-            }
-
-            if (userAnswer == 1){
-                  System.out.printf("\nPesquisa!\n\n");
-                  System.out.printf("Qual passaro você deseja pesquisar?\n");
-
-                  //ave1.searchName(sc);
-                  //ave1.showBird();
-            }
-            else if(userAnswer == 2){
-                  System.out.printf("\nAnotação!\n\n");
-                  System.out.printf("O que você viu?\n");
-
-                  //caderno.newRegister();
-            }
-
+            System.out.println(ave1.getName());
 
       }
 
